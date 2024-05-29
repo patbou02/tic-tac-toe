7f2a91b6a77a6f1476517c33138b22e4569c3258
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-05-29 15:37:41 -0700

    Reduced state management
    Removed unnecessary state activePlayer and instead copied code from within handleSelectSquare function to set currentPlayer to appropriate value based on already existing gameTurns state.

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import Log from './components/Log.jsx';
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
+
+  let currentPlayer = 'X';
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') currentPlayer = 'O';
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer(currentActivePlayer => currentActivePlayer === 'X' ? 'O' : 'X');
     setGameTurns(prevTurns => {
       let currentPlayer = 'X';
       if (prevTurns.length > 0 && prevTurns[0].player === 'X') currentPlayer = 'O';
@@ -23,8 +24,8 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
+          <Player initialName="Player 1" symbol="X" isActive={currentPlayer === 'X'} />
+          <Player initialName="Player 2" symbol="O" isActive={currentPlayer === 'O'} />
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
