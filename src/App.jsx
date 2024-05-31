147de48f45e921127986fea19b435781cdd7369f
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-05-30 22:34:07 -0700

    Added gameover component with winner prop and logic to track winner or draw result.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Player from './components/Player.jsx';
 import GameBoard from './components/GameBoard.jsx';
 import Log from './components/Log.jsx';
 import { WINNING_COMBINATIONS } from './winning-combinations.js';
+import GameOver from './components/GameOver.jsx';
 
 const initialGameBoard = [
   [null, null, null],
@@ -46,6 +47,8 @@ function App() {
     }
   }
 
+  const hasDraw = gameTurns.length === 9 && !winner;
+
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns(prevTurns => {
       const currentPlayer = derivedCurrentPlayer(prevTurns);
@@ -63,7 +66,7 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={currentPlayer === 'X'} />
           <Player initialName="Player 2" symbol="O" isActive={currentPlayer === 'O'} />
         </ol>
-        {winner && <p>You won, {winner}!</p>}
+        {(winner || hasDraw) && <GameOver winner={winner} />}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
