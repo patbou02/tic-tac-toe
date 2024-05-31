484c061756764690e8dcbeed3926b52a95d96a3f
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-05-30 22:45:57 -0700

    Handle restart button click and bug fix with initial game board variable
    Created restart function which sets gameTurns state back to empty array  then passed down to GameOver via new prop. Fixed bug in initial gameBoard variable to be set as exact copy of initialGameBoard rather than overwritting starting initialGameBoard variable.

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
 
   const currentPlayer = derivedCurrentPlayer(gameTurns);
 
-  let gameBoard = initialGameBoard;
+  let gameBoard = [...initialGameBoard.map(array => [...array])];
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -59,6 +59,10 @@ function App() {
     });
   }
 
+  function handleRestart() {
+    setGameTurns([]);
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -66,7 +70,7 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={currentPlayer === 'X'} />
           <Player initialName="Player 2" symbol="O" isActive={currentPlayer === 'O'} />
         </ol>
-        {(winner || hasDraw) && <GameOver winner={winner} />}
+        {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart} />}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
